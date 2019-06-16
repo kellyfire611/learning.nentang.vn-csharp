d16bac3ccf5e8161e1b26be5cc67e57c662d05f2
kellyfire611 <kellyfire611@gmail.com>
2019-06-17 05:39:00 +0700

    Thêm bài OOP - TeacherAndStudent

---
+++ src/TeacherAndStudentOOP/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeacherAndStudentOOP
+{
+    class Person
+    {
+        protected int age;
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void SetAge(int n)
+        {
+            age = n;
+        }
+    }
+
+    class Student : Person
+    {
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is: {0} years old", age);
+        }
+    }
+
+    class Teacher : Person
+    {
+        private string subject;
+
+        public void Explain()
+        {
+            Console.WriteLine("Explanation begins");
+        }
+    }
+
+    class StudentAndTeacherTest
+    {
+        static void Main()
+        {
+            bool debug = false;
+
+            Person myPerson = new Person();
+            myPerson.Greet();
+
+            Student myStudent = new Student();
+            myStudent.SetAge(21);
+            myStudent.Greet();
+            myStudent.ShowAge();
+
+            Teacher myTeacher = new Teacher();
+            myTeacher.SetAge(30);
+            myTeacher.Greet();
+            myTeacher.Explain();
+
+            if (debug)
+                Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
