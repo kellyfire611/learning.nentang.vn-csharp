70e19e72c87b53fe674c878d3109907a21adadc0
kellyfire611 <kellyfire611@gmail.com>
2019-10-30 11:53:23 +0700

    Bổ sung code bài tập Operator C#

--- src/OperatorInCSharp/Program.cs
+++ src/OperatorInCSharp/Program.cs
@@ -48,6 +48,29 @@ namespace OperatorInCSharp
             Console.WriteLine(++i); //Prints "2", i = 2. Pre-Incrementation
             Console.WriteLine(i--); //Prints "2", i = 1. Post-Decrementation
             Console.WriteLine(--i); //Prints "0", i = 0. Pre-Decrementation
+
+            //
+            // TODO: Viết chương trình tính Hằng đẳng thức
+            // https://vi.wikipedia.org/wiki/B%E1%BA%A3y_h%E1%BA%B1ng_%C4%91%E1%BA%B3ng_th%E1%BB%A9c_%C4%91%C3%A1ng_nh%E1%BB%9B
+            // Write a program to calculate (a+b)2.
+            // Hint: (a * a + b * b + 2 * a * b)
+            //
+
+
+
+
+            //
+            // TODO: Viết chương trình tính Diện tích hình tròn
+            // https://www.mathsisfun.com/geometry/circle-area.html
+            // Write a program to calculate formulae A = (π * r * 2).
+            //
+
+
+
+            //
+            // TODO: Viết chương trình tính Diện tích Tam giác
+            // https://www.mathsisfun.com/algebra/trig-area-triangle-without-right-angle.html
+            //
         }
     }
 }
