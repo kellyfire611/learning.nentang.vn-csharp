321a652ed1ddb1f18c017129db1f7bfc7f460b00
kellyfire611 <kellyfire611@gmail.com>
2019-11-03 21:37:37 +0700

    Bổ sung Bảng cửu chương

---
+++ src/BangCuuChuong/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BangCuuChuong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("=== BANG CUU CHUONG ===");
+            
+            // Thu thập thông tin Người dùng
+            Console.Write("Ban muon in ra bao nhieu bang cuu chuong? ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            // In bảng cửu chương
+            // Vòng lặp thứ 1 => lặp từ 1 đến num
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine("--- Cuu chuong {0} ---", i);
+                // Vòng lặp con nằm bên trong => lặp từ 1 -> 10
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", i, j, (i * j));
+                }
+                Console.WriteLine("--- Ket thuc cuu chuong ---");
+            }
+
+            // Dừng màn hình
+            Console.ReadLine();
+        }
+    }
+}
